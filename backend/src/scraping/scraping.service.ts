1daebf6414f2b06776ea88ef7da84b08c3bc3ab9
Med-Ri <harhira.mouhamedridha@gmail.com>
2026-09-20 20:17:23 +0100

    feat(scraping): add createJob method

--- backend/src/scraping/scraping.service.ts
+++ backend/src/scraping/scraping.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class ScrapingService {
@@ -13,5 +14,26 @@ export class ScrapingService {
         this.scraperBaseUrl =
             this.configService.get<string>('SCRAPER_BASE_URL') ??
             '';
+    }
+
+    async createJob() {
+        const response = await firstValueFrom(
+            this.httpService.post(`${this.scraperBaseUrl}/api/v1/jobs`, {
+                name: 'placescout-test',
+                keywords: ['Italian restaurants'],
+                lang: 'en',
+                zoom: 14,
+                lat: '48.8566',
+                lon: '2.3522',
+                fast_mode: true,
+                radius: 5000,
+                depth: 1,
+                email: false,
+                max_time: 60,
+                proxies: [],
+            }),
+        );
+
+        return response.data;
     }
 }
