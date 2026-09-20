a7adb23f65e4f870593a635506568e0e9b8079d9
Med-Ri <harhira.mouhamedridha@gmail.com>
2026-09-20 22:25:31 +0100

    feat(scraping): import geocoding module

--- backend/src/scraping/scraping.module.ts
+++ backend/src/scraping/scraping.module.ts
@@ -4,11 +4,13 @@ import { HttpModule } from '@nestjs/axios';
 import { ScrapingController } from './scraping.controller';
 import { ScrapingService } from './scraping.service';
 import { BusinessesModule } from '../businesses/businesses.module';
+import { GeocodingModule } from '../geocoding/geocoding.module';
 
 @Module({
   imports: [
     HttpModule,
     BusinessesModule,
+    GeocodingModule,
   ],
   controllers: [ScrapingController],
   providers: [ScrapingService],
