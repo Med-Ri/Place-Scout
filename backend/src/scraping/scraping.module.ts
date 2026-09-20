f79ef1cf7f5400e52e769332ee6a9ae41ddfc110
Med-Ri <harhira.mouhamedridha@gmail.com>
2026-09-20 19:12:51 +0100

    feat(scraping): configure http client

--- backend/src/scraping/scraping.module.ts
+++ backend/src/scraping/scraping.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+
 import { ScrapingController } from './scraping.controller';
 import { ScrapingService } from './scraping.service';
 
 @Module({
+  imports: [HttpModule],
   controllers: [ScrapingController],
-  providers: [ScrapingService]
+  providers: [ScrapingService],
 })
 export class ScrapingModule {}
--- backend/src/scraping/scraping.service.ts
+++ backend/src/scraping/scraping.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
 
 @Injectable()
-export class ScrapingService {}
+export class ScrapingService {
+    constructor(private readonly httpService: HttpService) { }
+}
