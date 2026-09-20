20f061885c594bd956c1e89556de579764a8eb0b
Med-Ri <harhira.mouhamedridha@gmail.com>
2026-09-20 22:26:11 +0100

    feat(geocoding): export GeocodingService

--- backend/src/geocoding/geocoding.module.ts
+++ backend/src/geocoding/geocoding.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+
 import { GeocodingService } from './geocoding.service';
 
 @Module({
-  providers: [GeocodingService]
+  providers: [GeocodingService],
+  exports: [GeocodingService],
 })
 export class GeocodingModule {}
