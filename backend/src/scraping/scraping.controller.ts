cdd65d8016d64c16c56b7697644ac12fa307eae6
Med-Ri <harhira.mouhamedridha@gmail.com>
2026-09-20 20:21:27 +0100

    feat(scraping): add endpoint to test job creation

--- backend/src/scraping/scraping.controller.ts
+++ backend/src/scraping/scraping.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { ScrapingService } from './scraping.service';
 
 @Controller('scraping')
-export class ScrapingController {}
+export class ScrapingController {
+    constructor(
+        private readonly scrapingService: ScrapingService,
+    ) { }
+
+    @Post('test')
+    createTestJob() {
+        return this.scrapingService.createJob();
+    }
+}
