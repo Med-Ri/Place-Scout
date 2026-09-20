790d96ea7290b16d9b88a587c60291b5411e82e7
Med-Ri <harhira.mouhamedridha@gmail.com>
2026-09-20 15:48:38 +0100

    feat(businesses): add create and list endpoints

--- backend/src/businesses/businesses.controller.ts
+++ backend/src/businesses/businesses.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+
+import { BusinessesService } from './businesses.service';
+import { Business } from './schemas/business.schema';
 
 @Controller('businesses')
-export class BusinessesController {}
+export class BusinessesController {
+    constructor(
+        private readonly businessesService: BusinessesService,
+    ) { }
+
+    @Post()
+    create(@Body() data: Partial<Business>) {
+        return this.businessesService.create(data);
+    }
+
+    @Get()
+    findAll() {
+        return this.businessesService.findAll();
+    }
+}
