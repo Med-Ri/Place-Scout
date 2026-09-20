de0bd10e334e876b38b7e3e7ce5feac3a8b01c80
Med-Ri <harhira.mouhamedridha@gmail.com>
2026-09-20 19:15:21 +0100

    feat(scraping): configure scraper base URL

--- backend/src/scraping/scraping.service.ts
+++ backend/src/scraping/scraping.service.ts
@@ -1,7 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { HttpService } from '@nestjs/axios';
 
 @Injectable()
 export class ScrapingService {
-    constructor(private readonly httpService: HttpService) { }
+    private readonly scraperBaseUrl: string;
+
+    constructor(
+        private readonly httpService: HttpService,
+        private readonly configService: ConfigService,
+    ) {
+        this.scraperBaseUrl =
+            this.configService.get<string>('SCRAPER_BASE_URL') ??
+            '';
+    }
 }
