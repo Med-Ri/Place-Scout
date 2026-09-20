e8efe3c06cc7fd990a4d52004b278be4a39f0dc1
Med-Ri <harhira.mouhamedridha@gmail.com>
2026-09-20 15:39:15 +0100

    feat(backend): configure mongodb and environment

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { ScrapingModule } from './scraping/scraping.module';
 
 @Module({
-  imports: [ScrapingModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    MongooseModule.forRoot(process.env.MONGODB_URI!),
+
+    ScrapingModule,
+  ],
 })
 export class AppModule {}
